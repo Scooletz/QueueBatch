ae36eb0ea344a9b7998e008094f1cfccbbc09580
Zhongming Chen <zhongmingc@asos.com>
2018-12-19 11:10:32 +0000

    Replace HashSet to use ConcurrentBag for multi-threading support

--- src/QueueBatch/Impl/MessageBatch.cs
+++ src/QueueBatch/Impl/MessageBatch.cs
@@ -1,5 +1,7 @@
 ﻿using System;
+using System.Collections.Concurrent;
 using System.Collections.Generic;
+using System.Linq;
 using System.Threading;
 using System.Threading.Tasks;
 using QueueBatch.Impl.Queues;
@@ -12,9 +14,9 @@ namespace QueueBatch.Impl
         readonly QueueFunctionLogic queue;
         readonly int maxDequeueCount;
         readonly TimeSpan visibilityTimeout;
-        readonly HashSet<string> processed;
+        readonly ConcurrentBag<string> processed;
 
-        static readonly HashSet<string> Empty = new HashSet<string>();
+        static readonly ConcurrentBag<string> Empty = new ConcurrentBag<string>();
 
         public MessageBatch(Message[] messages, QueueFunctionLogic queue, int maxDequeueCount, TimeSpan visibilityTimeout)
         {
@@ -22,7 +24,7 @@ namespace QueueBatch.Impl
             this.queue = queue;
             this.maxDequeueCount = maxDequeueCount;
             this.visibilityTimeout = visibilityTimeout;
-            processed = new HashSet<string>();
+            processed = new ConcurrentBag<string>();
         }
 
         public IEnumerable<Message> Messages => messages;
@@ -41,14 +43,16 @@ namespace QueueBatch.Impl
 
         public Task RetryAll(CancellationToken ct) => Complete(ct, Empty);
 
-        Task Complete(CancellationToken ct, HashSet<string> processed)
+        Task Complete(CancellationToken ct, IEnumerable<string> processed)
         {
             var tasks = new Task[messages.Length];
+            var messagesToDelete = processed.ToList();
+
             for (var i = 0; i < messages.Length; i++)
             {
                 var message = messages[i];
 
-                if (processed.Contains(message.Id))
+                if (messagesToDelete.Contains(message.Id))
                 {
                     tasks[i] = queue.DeleteMessage(message, ct);
                 }
